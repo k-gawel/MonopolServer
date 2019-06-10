3367ae764c267cc082b2cc51e20b1fa011d4070a
Kamil <kamil.gawel.aplikacje@gmail.com>
2019-06-10 16:09:38 +0200

    Add PlayerLeaveService.java

---
+++ src/main/java/org/california/monopolserver/service/action/PlayerLeaveService.java
@@ -0,0 +1,84 @@
+package org.california.monopolserver.service.action;
+
+import org.california.monopolserver.instance.executable.transaction.Transaction;
+import org.california.monopolserver.instance.executable.transaction.VoluntaryTransaction;
+import org.california.monopolserver.instance.game.Game;
+import org.california.monopolserver.instance.player.Player;
+import org.california.monopolserver.instance.player.Session;
+import org.california.monopolserver.instance.transferable.town.Town;
+import org.california.monopolserver.instance.transferable.utility.Utility;
+import org.california.monopolserver.model.ws_message.request.action.PlayerLeaveRequest;
+import org.california.monopolserver.model.ws_message.response.player.PlayerLeaveResponse;
+import org.california.monopolserver.service.CustomMessageTemplate;
+import org.california.monopolserver.service.game.GameRegistry;
+import org.california.monopolserver.service.transaction.result.TransactionCommitService;
+import org.california.monopolserver.utils.annotations.RequestProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PlayerLeaveService {
+
+    private final TransactionCommitService commitService;
+    private final CustomMessageTemplate messageTemplate;
+
+    @Autowired
+    public PlayerLeaveService(TransactionCommitService commitService, CustomMessageTemplate messageTemplate) {
+        this.commitService = commitService;
+        this.messageTemplate = messageTemplate;
+    }
+
+    @RequestProcessor(PlayerLeaveRequest.class)
+    public void leave(PlayerLeaveRequest request) {
+        Player player = GameRegistry.getPlayerByUuid(new Session(request.session).playerUuid);
+        removePlayer(player);
+    }
+
+
+    public void removePlayer(Player player) {
+        Game game = player.getGame();
+
+        if(game.currentTour != null && game.currentTour.player.equals(player))
+            return;
+
+        if(game.players.size() <= 2)
+            abortGame(player);
+
+        setNextAdmin(game);
+        sellAllPlayersProperties(player);
+        game.players.remove(player);
+
+        PlayerLeaveResponse response = new PlayerLeaveResponse(player, null, null, false);
+        messageTemplate.sendMessage(response);
+    }
+
+
+    private void setNextAdmin(Game game) {
+        game.admin = game.players.get(1);
+    }
+
+
+    private void sellAllPlayersProperties(Player player) {
+        Transaction transaction = new VoluntaryTransaction(player, player.getGame().bank);
+
+        player.properties.get(Town.class).forEach(t -> transaction.add(player, t));
+        player.properties.get(Utility.class).forEach(t -> transaction.add(player, t));
+
+        commitService.commitTransaction(transaction);
+    }
+
+
+    private void abortGame(Player player) {
+        List<Player> players = player.getGame().players;
+        players.remove(player);
+        Player winner = players.isEmpty() ? null : players.get(0);
+        Player loser  = player;
+
+        PlayerLeaveResponse response = new PlayerLeaveResponse(player, winner, loser, true);
+        messageTemplate.sendMessage(response);
+    }
+
+
+}
